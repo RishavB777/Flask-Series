aec2c047fe51c1aa05c8cd82e49a045aad39c7e5
RISHAV BHATTACHARJEE <rishavbhattacharjee777@gmail.com>
2021-06-16 21:25:23 +0530

    Add files via upload

---
+++ 005_integrating_css_and_javascript.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('submit.html')
+
+@app.route('/submit',methods=["POST","GET"])
+def submit():
+    if request.method=="POST":
+        avg = (float(request.form['science'])+float(request.form['c'])+float(request.form['datascience'])+float(request.form['maths']))/4
+    if avg > 30:
+        res = "PASSED"
+    else:
+        res = "FAILED"
+    return render_template("result.html",result=res,marks=avg)
+
+if __name__ == "__main__":
+    app.run(debug=True)
